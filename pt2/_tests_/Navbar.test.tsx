436f31440b26fdd1a7e56c6b3660b7c0ab73fff0
sumit vikram <vikramsumit@outlook.com>
2025-08-02 23:01:30 +0530

    Initial commit

--- pt2/_tests_/Navbar.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Navbar from '../components/Navbar';
-
-describe('Navbar', () => {
-  it('renders site name', () => {
-    render(<Navbar />);
-    expect(screen.getByText('Raju Raj')).toBeInTheDocument();
-  });
-});
--- pt2/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-// module.exports = {
-//   reactStrictMode: true,
-// };
-module.exports = {
-  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
-}
