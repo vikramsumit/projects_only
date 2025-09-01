727a2cbe98e87583c2229f89ced0f6222ad4b1ae
sumit vikram <vikramsumit@outlook.com>
2025-09-01 15:14:56 +0530

    Initial commit

---
+++ music/practical.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid;
+
+    printf("Parent process started. PID = %d\n", getpid());
+
+    pid = fork();
+
+    if (pid < 0) {
+        perror("fork failed");
+        exit(1);
+    } 
+    else if (pid == 0) {
+        printf("Child process created. PID = %d, Parent PID = %d\n", getpid(), getppid());
+
+        printf("Child executing 'ls -l' using exec...\n");
+        execl("/bin/ls", "ls", "-l", NULL);
+
+        perror("exec failed");
+        exit(1);
+    } 
+    else {
+        printf("Parent waiting for child (PID = %d)...\n", pid);
+
+        int status;
+        wait(&status);
+
+        if (WIFEXITED(status)) {
+            printf("Child exited normally with status = %d\n", WEXITSTATUS(status));
+        } else {
+            printf("Child terminated abnormally.\n");
+        }
+
+        printf("Parent process ends.\n");
+    }
+
+    return 0;
+}
+
