cbe833160f53c19732be38eefe763387e1fa6f40
sumit vikram <vikramsumit@outlook.com>
2025-09-30 13:31:09 +0530

    Initial commit

---
+++ basic/To-Do/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="bg-red-600">Hey I am blue</div>
+      </div>
+    </>
+  )
+}
+
+export default App
